2e71dae0cb8bdc326fe9626800aeb0c6f75639ac
Evertte Berkyaw <berkyawevertte@gmail.com>
2025-11-14 16:41:06 -0600

    I modified the main.py file to include new functionality for handling user input and processing data more efficiently.

---
+++ main.py
@@ -0,0 +1,25 @@
+from dotenv import load_dotenv
+from typing import Annotated
+from langgraph.graph import StateGraph, Start, End
+from langgraph.graph.message import add_messages
+from langchain.chat_models import init_chat_model
+from typing_extensions import TypedDict
+from pydantic import BaseModel, Field
+from typing import List
+
+load_dotenv()
+
+llm = init_chat_model("gpt-4o")
+
+class State(TypedDict):
+    messages: Annotated[List[str], add_messages]
+    user_question: str | None
+    google_results: str | None
+    bing_results: str | None
+    reddit_results: str | None
+    selected_reddit_urls: list[str] | None
+    reddit_post_data: str | None
+    google_analysis: str | None
+    bing_analysis: str | None
+    reddit_analysis: str | None
+    final_answer: str | None
